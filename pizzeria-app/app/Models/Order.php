1be55c29dc72b9e0f0e10e0e26db0e954a592dac
LEMMEL <LEMMEL@LEMMEL>
2025-04-07 23:13:36 -0500

    Modelo Order

--- pizzeria-app/app/Models/Order.php
+++ pizzeria-app/app/Models/Order.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'client_id',
+        'branch_id',
+        'total_price',
+        'status',
+        'delivery_type',
+        'delivery_person_id',
+    ];
+
+    protected $casts = [
+        'total_price' => 'decimal:2',
+    ];
+
+    // Relaciones
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function deliveryPerson()
+    {
+        return $this->belongsTo(Employee::class, 'delivery_person_id');
+    }
+
+    public function extraIngredients()
+    {
+        return $this->belongsToMany(ExtraIngredient::class, 'order_extra_ingredient');
+    }
 }
