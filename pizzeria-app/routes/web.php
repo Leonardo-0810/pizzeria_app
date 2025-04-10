391838e577a791c1dc8de09a0f4bc3241440c37c
JUAN OCAMPO SALAZAR <ocampojuan035@gmail.com>
2025-04-09 22:06:49 -0500

    se suben los cambios para el crud ingredients

--- pizzeria-app/routes/web.php
+++ pizzeria-app/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\PizzaController;
+use App\Http\Controllers\IngredientController; 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,12 +20,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+    Route::resource('pizzas', PizzaController::class);
     Route::resource('clients', ClientController::class);
-
+    Route::resource('ingredients', IngredientController::class);
     Route::resource('employees', EmployeeController::class);
 
     
 });
-Route::resource('pizzas', PizzaController::class);
+
 require __DIR__.'/auth.php';
